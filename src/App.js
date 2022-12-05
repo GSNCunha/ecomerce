6c32ebb9f0862894a6b4f9a3474bf37a22d780a0
GSNCunha <61071956+GSNCunha@users.noreply.github.com>
2022-12-05 00:04:45 -0300

    first complete header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import React, { Component } from 'react';
 
 import Header from './Components/Header';
+import Product from './Components/Product';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
+        <Header />
+        <Product />
+        
       </div>
     );
   }
