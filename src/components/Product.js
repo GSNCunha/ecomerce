829cfa05fcfaea5714e5ffdaf674fb5eab7e5289
code <code@Gabriels-Mac-mini.local>
2022-12-12 06:25:33 -0300

    only the cart counter is not ok

--- src/components/Product.js
+++ src/components/Product.js
@@ -35,9 +35,31 @@ class Header extends Component {
               <h4 className='sneakerCompany text'>SNEAKER COMPANY</h4>
               <h1 className='title text'>Fall Limited Edition Sneakers</h1>
               <p className='paragraph text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-              <div className='a'><div className='price'><p className='priceText'>$125</p></div><div className='discount'><p className='textdiscount'>50%</p></div></div>
+              <div className='a'>
+                <div className='price'>
+                  <p className='priceText'>$125</p>
+                </div>
+                <div className='discount'>
+                  <p className='textdiscount'>50%</p>
+                </div>
+              </div>
               <div className='linetextspacing'><h7 className="line">$150</h7></div>
-              <div className='cartcontainer'><div className='numerOfItems'></div><div className='AddToCart'></div></div>
+              <div className='cartcontainer'>
+                <div className='numerOfItems'>
+                  <div className='plus'>
+                    <div className='TextPlus'><p className='text pluscolor'>a</p></div>
+                  </div>
+                  <div className='counter'></div>
+                  <div className='minus'></div>
+                </div>
+                <div className='AddToCart text'>
+                  <p className='TextCart'>
+                  <svg width="22" height="20" className='cartLogo' xmlns="http://www.w3.org/2000/svg"><path d="M20.925 3.641H3.863L3.61.816A.896.896 0 0 0 2.717 0H.897a.896.896 0 1 0 0 1.792h1l1.031 11.483c.073.828.52 1.726 1.291 2.336C2.83 17.385 4.099 20 6.359 20c1.875 0 3.197-1.87 2.554-3.642h4.905c-.642 1.77.677 3.642 2.555 3.642a2.72 2.72 0 0 0 2.717-2.717 2.72 2.72 0 0 0-2.717-2.717H6.365c-.681 0-1.274-.41-1.53-1.009l14.321-.842a.896.896 0 0 0 .817-.677l1.821-7.283a.897.897 0 0 0-.87-1.114ZM6.358 18.208a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm10.015 0a.926.926 0 0 1 0-1.85.926.926 0 0 1 0 1.85Zm2.021-7.243-13.8.81-.57-6.341h15.753l-1.383 5.53Z" fill="#69707D" fill-rule="nonzero"/></svg>
+                         Add to cart
+                  </p>
+                  
+                </div>
+              </div>
             </div>
           </div>
         </div>
