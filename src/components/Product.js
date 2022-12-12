d30e6162a5719740a5647f2bf43e0c96c8737b57
code <code@Gabriels-Mac-mini.local>
2022-12-12 04:08:07 -0300

    grid of images style working

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-
+import mainproduct from './images/image-product-1.jpg';
+import product1 from "./images/image-product-1-thumbnail.jpg";
+import product2 from "./images/image-product-2-thumbnail.jpg";
+import product3 from "./images/image-product-3-thumbnail.jpg";
+import product4 from "./images/image-product-4-thumbnail.jpg";
 
 class Header extends Component {
     render() {
@@ -9,23 +13,21 @@ class Header extends Component {
         <div className='container2'>
           <div className='container3'>
             <div >
-              <div className='mainproduct' src="images\image-product-1-thumbnail.jpg" alt="product main photo" ></div>
+              <div className='mainproduct' ><img className='photoMain' src={mainproduct} alt="main product photo" width="480" height="480"></img></div>
             </div>
             <div className='container4'>
-              <div className='space'/>
               <div className='product'>
-
+                <img className='photoProduct' src={product1} alt="product 1 thumbmail " width="100" height="100"></img>
               </div>
               <div className='product'>
-                
+              <img className='photoProduct' src={product2} alt="product 2 thumbmail " width="100" height="100"></img>
               </div>
               <div className='product'>
-                
+              <img className='photoProduct' src={product3} alt="product 3 thumbmail " width="100" height="100"></img>
               </div>
               <div className='product'>
-                
+              <img className='photoProduct' src={product4} alt="product 4 thumbmail " width="100" height="100"></img>
               </div>
-              <div className='space'/>
             </div>
           </div>
           <div>
